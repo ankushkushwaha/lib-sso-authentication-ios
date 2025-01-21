8092ae2a89ab21945571dbb27161a47e5cad1a11
ankushkushwaha <ankush7377@gmail.com>
2025-01-21 13:56:26 +0530

    Add sample demo project

---
+++ DemoProject/DemoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by Ankush Kushwaha on 21/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoProject/DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by Ankush Kushwaha on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
