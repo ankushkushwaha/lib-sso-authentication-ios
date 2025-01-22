e837c13dcc1fe16dd6f8ba4293a9ae559d608cba
ankushkushwaha <ankush7377@gmail.com>
2025-01-22 11:36:02 +0530

    Add Example project

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Ankush Kushwaha on 22/01/25.
+//
+
+import SwiftUI
+import AppAuth
+import lib_sso_authentication_ios
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
